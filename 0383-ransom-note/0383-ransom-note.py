038383a6cb7d9a38330a026856b7237dce2a868d
Natnael Malike <112170027+NatnaelMalike@users.noreply.github.com>
2024-09-10 01:28:42 +0300

    Time: 35 ms (98.19%), Space: 16.6 MB (65.03%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,12 +1,7 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        maga = Counter(magazine)
-        rans = Counter(ransomNote)
-        for key in rans:
-            if key not in maga:
-                return False
-            elif maga[key] < rans[key]:
-                return False
+        for letter in list(set(ransomNote)):
+            if ransomNote.count(letter) > magazine.count(letter): return False
         return True
 
         
