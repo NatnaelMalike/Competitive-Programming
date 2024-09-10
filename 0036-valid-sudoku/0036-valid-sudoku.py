439579945bcf320172713d1bdf9ead827a951c79
Natnael Malike <112170027+NatnaelMalike@users.noreply.github.com>
2024-09-10 22:59:57 +0300

    Time: 89 ms (74.95%), Space: 16.5 MB (52.56%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,11 +1,33 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        res = []
         for i in range(9):
+            uniq = []
             for j in range(9):
-                if board[i][j] != '.':
-                    res += [(i, board[i][j]), (board[i][j], j), (i // 3, j // 3, board[i][j])]
-        return len(res) == len(set(res))
+                if board[i][j] in uniq:
+                    return False
+                elif board[i][j] != '.':
+                    uniq.append(board[i][j])
+        for i in range(9):
+            uniq = []
+            for j in range(9):
+                if board[j][i] in uniq:
+                    return False
+                elif board[j][i] != '.':
+                    uniq.append(board[j][i])
+        starter = [
+            [0,0], [0,3], [0,6],
+            [3,0], [3,3], [3,6],
+            [6,0], [6,3], [6,6]
+        ]
+        for i,j in starter:
+            uniq = []
+            for r in range(i, i+3):
+                for c in range(j, j+3):
+                    if board[r][c] in uniq:
+                        return False
+                    elif board[r][c] != '.':
+                        uniq.append(board[r][c])
+        return True
 
                 
         
