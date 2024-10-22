222aef74556e00e427b59f50eeea8d3c80d559e0
Natnael Malike <112170027+NatnaelMalike@users.noreply.github.com>
2024-10-22 18:12:06 +0300

    Time: 58 ms (99.72%), Space: 16.9 MB (96.89%) - LeetHub

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/1469-minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/1469-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -3,10 +3,13 @@ class Solution:
         dic_s = Counter(s)
         dic_t = Counter(t)
         steps = 0
-        for key,value in dic_s.items():
-            if key in dic_t and value > dic_t[key]:
-                steps += value - dic_t[key]
-            elif(key not in dic_t):
+        
+        for key, value in dic_s.items():
+            if key in dic_t:
+                if value > dic_t[key]:
+                    steps += value - dic_t[key]
+            else:
                 steps += value
+                
         return steps
 
