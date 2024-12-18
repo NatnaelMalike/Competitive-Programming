2d1c114396f3bdbebfed8e9d7dbf0fb1e6bc235e
Natnael Malike <112170027+NatnaelMalike@users.noreply.github.com>
2024-12-18 10:34:53 +0300

    Time: 39 ms (76.56%), Space: 18.4 MB (5.89%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
@@ -3,7 +3,6 @@ class Solution:
         n = len(nums)
         one_freq = 0
         l = 0
-        max_len = 0
         for r in range(n):
             if nums[r] == 1:
                 one_freq += 1
@@ -11,6 +10,5 @@ class Solution:
                 if nums[l] == 1:
                     one_freq -= 1
                 l += 1
-            max_len = max(max_len, r - l + 1)
-        return max_len
+        return n - l
         
