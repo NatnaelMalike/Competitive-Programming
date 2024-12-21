9744cc618565d29bb2dfc6cf4eb581b7fa716aa8
Natnael Malike <112170027+NatnaelMalike@users.noreply.github.com>
2024-12-22 01:19:31 +0300

    Time: 7 ms (30.77%), Space: 17.9 MB (6.27%) - LeetHub

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.py
@@ -0,0 +1,15 @@
+class Solution:
+    def decrypt(self, code: List[int], k: int) -> List[int]:
+        n = len(code)
+        if k == 0:
+            return [0] * n
+        
+        result = [0] * n
+        
+        for i in range(n):
+            if k > 0:
+                result[i] = sum(code[(i + j) % n] for j in range(1, k + 1))
+            else:
+                result[i] = sum(code[(i - j) % n] for j in range(1, -k + 1))
+        
+        return result
