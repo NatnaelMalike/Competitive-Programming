8fea8407d0f9065b862601b117eb7cb0a12ed838
Natnael Malike <112170027+NatnaelMalike@users.noreply.github.com>
2025-03-07 21:54:54 +0300

    Time: 93 ms (37.67%), Space: 17.2 MB (100%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,18 +1,8 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        n = len(heights)
-        for i in range(n-1):
-            for j in range(i+1,n):
-                if heights[i] < heights[j]:
-                    heights[i], heights[j] = heights[j], heights[i]
-                    names[i], names[j] = names[j], names[i]
-
-        return names
-
-        
-       
-
-
-   
-
+        per_to_hei = {}
+        for i in range(len(names)):
+            per_to_hei[heights[i]] = names[i]
+        sortedPer = dict(sorted(per_to_hei.items()))
+        return reversed(sortedPer.values())
         
